51afe6adbb63d3946253e0ca83e0cb47bc06635e
a l C o P a U L <paul.amurao@gmail.com>
2024-09-23 14:57:17 -0400

    Update csp.asm
    RTM.FINAL

--- csp.asm
+++ csp.asm
@@ -16,7 +16,7 @@ main:sub rsp,28h
 mov al,0Ah
 mov bl,5Fh
 z:lea r9,[a]
-mov dx,1370
+mov dx,1372
 r:cmp byte[r9],bl
 je s
 jmp u
@@ -85,13 +85,13 @@ section .data
 b db 27h,0Dh,0Ah,64h,62h,20h,27h,0
 x db 2Ch,32h,37h,68h,2Ch,30h,0
 i db 25h,73h,0
-n db 1370 DUP(0)
+n db 1372 DUP(0)
 a db '; /////////////////////////////////////////////////////////////////////_; ////////////////////////////////////////////////////////////////_;_; csp.a'
 db 'sm_; by alCoPaUL [GIMO][As][aBrA][NPA][b8][BCVG][rRlf]_; 2/27/2023 Revised: 9/23/2024 @ NYC_; nasm -f elf64 csp.asm_; bcc64 csp.o_;_; ////////////////'
 db '///////////////////////////////////////////////////////////_; /////////////////////////////////////////////////////////////////////_global main_extern'
-db ' printf_section .text_main:sub rsp,28h_mov al,0Ah_mov bl,5Fh_z:lea r9,[a]_mov dx,1370_r:cmp byte[r9],bl_je s_jmp u_s:mov byte[r9],al_u:inc r9_dec dx_c'
+db ' printf_section .text_main:sub rsp,28h_mov al,0Ah_mov bl,5Fh_z:lea r9,[a]_mov dx,1372_r:cmp byte[r9],bl_je s_jmp u_s:mov byte[r9],al_u:inc r9_dec dx_c'
 db 'mp dx,0_jnz r_cmp bl,0Ah_je td_lea rdx,[a]_lea rcx,[i]_call printf_jmp ll_td:_lea rdi,[n]_lea rsi,[a]_mov cx,1372_sn:mov al,byte[rsi]_mov byte[rdi],al'
 db '_inc rsi_inc rdi_dec cx_cmp cx,1224_je t_cmp cx,1074_je t_cmp cx,924_je t_cmp cx,774_je t_cmp cx,624_je t_cmp cx,474_je t_cmp cx,324_je t_cmp cx,174_j'
 db 'e t_cmp cx,24_je t_jmp sts_t:mov byte[rdi],27h_mov byte[rdi+1],0Ah_mov byte[rdi+2],64h_mov byte[rdi+3],62h_mov byte[rdi+4],20h_mov byte[rdi+5],27h_add'
 db ' rdi,6_jmp sts_sts:cmp cx,0_jne sn_lea rdx,[n]_lea rcx,[i]_call printf_ll:mov al,0Ah_cmp bl,5Fh_xchg al,bl_je z_lea rdx,[x]_lea rcx,[i]_call printf_ad'
-db 'd rsp,28h_ret_section .data_b db 27h,0Dh,0Ah,64h,62h,20h,27h,0_x db 2Ch,32h,37h,68h,2Ch,30h,0_i db 25h,73h,0_n db 1370 DUP(0)_a db ',27h,0
+db 'd rsp,28h_ret_section .data_b db 27h,0Dh,0Ah,64h,62h,20h,27h,0_x db 2Ch,32h,37h,68h,2Ch,30h,0_i db 25h,73h,0_n db 1372 DUP(0)_a db ',27h,0
